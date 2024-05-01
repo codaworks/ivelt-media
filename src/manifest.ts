c41c271171bb7aade4beccbc9c1c32bf52e5b5ed
Mordechai Meisels <mordechai@codaworks.com>
2024-05-01 11:30:42 -0400

    Created new Chrome extension with create-chrome-ext

---
+++ src/manifest.ts
@@ -0,0 +1,31 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageData from '../package.json'
+
+//@ts-ignore
+const isDev = process.env.NODE_ENV == 'development'
+
+export default defineManifest({
+  name: `${packageData.displayName || packageData.name}${isDev ? ` ➡️ Dev` : ''}`,
+  description: packageData.description,
+  version: packageData.version,
+  manifest_version: 3,
+  icons: {
+    16: 'img/logo-16.png',
+    32: 'img/logo-34.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
+  },
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*'],
+      js: ['src/index.ts'],
+      css: ['src/style.css']
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      matches: [],
+    },
+  ]
+})
