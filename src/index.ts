62febeb4dde67f54a31aebd2c1fbff0267cdd608
Mordechai Meisels <mordechai@codaworks.com>
2024-05-06 13:13:16 -0400

    Exclude Dropbox non-media files, resolve #1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+interface Media {
+    title: string
+    href: string
+    id: string
+    token?: string
+    filename?: string
+    type: "google-drive" | "dropbox"
+}
+
 const posts = document.querySelectorAll('.postprofile + .postbody .content, #preview .postbody .content')
 
 for (const post of posts) {
     const links = post.querySelectorAll(`
     .content > a[href^="https://drive.google.com/file/d/"],
     .content > :not(blockquote) a[href^="https://drive.google.com/file/d/"],
-    .content > a[href^="https://www.dropbox.com/"][href$="&dl=0"], 
-    .content > :not(blockquote) a[href^="https://www.dropbox.com"][href$="&dl=0"]
+    .content > a[href^="https://www.dropbox.com/scl/fi/"][href$="&dl=0"]:is([href*=".mp3"], [href*=".mp4"]), 
+    .content > :not(blockquote) a[href^="https://www.dropbox.com"][href$="&dl=0"]:is([href*=".mp3"], [href*=".mp4"])
     `)
     if (!links.length)
         continue
@@ -13,26 +22,42 @@ for (const post of posts) {
     const media = [...links].map(l => {
         const href = l.getAttribute('href') as string
 
-        let id: string | undefined
+        let id: string
         let type: 'google-drive' | 'dropbox'
+        let token: string | undefined
+        let filename: string | undefined
 
         if (href.startsWith('https://drive.google.com')) {
-            id = /^https:\/\/drive.google.com\/file\/d\/([^/]+)/.exec(href)?.[1]
+            const match = /^https:\/\/drive.google.com\/file\/d\/([^/]+)/.exec(href)!
+
+            id = match[1]
             type = 'google-drive'
         }
 
         else if (href.startsWith('https://www.dropbox.com')) {
-            id = /^https:\/\/www.dropbox.com\/(.+)\&dl=0/.exec(href)?.[1]
+            const match = /^https:\/\/www.dropbox.com\/scl\/fi\/([^/]+)\/(.+?)\?(.+)\&dl=0/.exec(href)!
+
+            token = match[1]
+            filename = match[2]
+            id = `${token}/${filename}?${match[3]}`
+
             type = 'dropbox'
         }
+
+        else
+            throw Error('Unknown media type')
+
 
         return {
             title: l.textContent ?? href,
             href,
             id,
-            type: type!
+            token,
+            filename,
+            type
         }
     })
+
 
 
     post.insertAdjacentHTML('afterend',
@@ -42,12 +67,14 @@ for (const post of posts) {
                 `<iframe 
                     src='https://drive.google.com/file/d/${m.id}/preview' 
                     frameborder='0' 
+                    loading='lazy'
+                    scrolling='no'
                     allowfullscreen>
                     Your browser does not support this content
                 </iframe>`
                 :
-                `<video controls preview='metadata'>
-                    <source src='https://dl.dropbox.com/${m.id}&dl=1'/>
+                `<video controls preview='metadata' filename=${m.filename}>
+                    <source src='https://dl.dropbox.com/scl/fi/${m.id}'/>
                     Your browser does not support this content
                 </video>`
             }
